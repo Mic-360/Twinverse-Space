a88136bdd19938cf0f803a6d1ef56f970af78040
Bhaumic-kun <bhaumiksingh2000@gmail.com>
2023-04-19 07:30:11 +0530

    Tfresh: home Route

---
+++ routes/index.tsx
@@ -0,0 +1,32 @@
+import { Head } from "$fresh/runtime.ts";
+import Button from "../components/Button.tsx";
+import Footer from "../islands/Footer.tsx";
+import Video from "../islands/Video.tsx";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>TwinVerse Technologies</title>
+        <link rel="icon" href="/logo.png" />
+      </Head>
+      <Video src="/background.mp4" type="video/mp4" />
+      <div class="flex justify-center items-center h-screen text-gray-600 drop-shadow-[0_5px_3px_rgba(255,255,255,0.8)]">
+        <div>
+          <div class="mb-4 mt-16 mx-8 text-center">
+            <span class="block text-5xl font-bold text-gray-300 mb-3 hover:text-white">
+              Making The World Work Better
+            </span>
+            <form
+              action="mailto:someone@example.com"
+              class="flex justify-end mt-8 mr-2"
+            >
+              <Button />
+            </form>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+}
