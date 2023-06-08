a4519cfb7cd8766c41e1a79dec2efa18d21b4dcf
Bhaumic <bhaumiksingh2000@gmail.com>
2023-06-08 10:30:56 +0000

    chore: assets refactor

--- islands/Footer.tsx
+++ islands/Footer.tsx
@@ -2,12 +2,13 @@ const Footer = () => {
   return (
     <div class="absolute bottom-0 left-0 flex px-4 justify-between w-full z-10">
       <span>
-        <a href="/">
+        <a href="/" class='flex justify-center item-center gap-x-4'>
           <img
           class="h-8 mx-auto mb-4 drop-shadow-2xl"
-          src="/logoImage.png"
+          src="/imgs/logoImage.svg"
           alt="Logo"
         />
+        <span class='text-white text-2xl font-semibold'>Twinverse™</span>
         </a>
       </span>
       <footer class="mr-10 flex gap-x-12 font-semibold text-gray-400">
