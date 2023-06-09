86b431a55427926f67f9659e799f45267fcfc9d9
Bhaumic <bhaumiksingh2000@gmail.com>
2023-06-09 08:12:36 +0000

    chore: deps fixes

--- twind.config.ts
+++ twind.config.ts
@@ -1,5 +1,12 @@
 import { Options } from "$fresh/plugins/twind.ts";
 
 export default {
-  selfURL: import.meta.url,
+  selfURL: import.meta.url, 
+  theme: {
+    extend: {
+      borderGradientColors: {
+        'yellow-blue': ['#f1ff55', '#386de3'],
+      },
+    },
+  },      
 } as Options;
