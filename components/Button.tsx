9cd804b92dd8ff0f10767f0fd2685efa07b3da49
Bhaumic <bhaumiksingh2000@gmail.com>
2023-06-09 08:11:02 +0000

    feat: pulse with no Footer bg image

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,9 +1,10 @@
 const Button = () => {
   return (
-    <button type='submit' class="flex justify-center items-center flex-col">
+    <button type='submit' class="flex justify-center items-center flex-col animate-pulse">
       <a
+        target='_blank'
         href="/"
-        class="bg-transparent border-gray-500 hover:border-gray-100 border-2 text-gray-400 hover:text-gray-100 shadow font-bold text-sm py-3 px-4 rounded-lg flex justify-start items-center cursor-pointer mt-4"
+        class="bg-transparent border-gradient-yellow-blue border-solid border-2 hover:border-gray-100 text-gray-400 hover:text-gray-100 shadow font-bold text-sm py-3 px-4 rounded-lg flex justify-start items-center cursor-pointer mt-4"
       >
         <span>Join The Waitlist</span>
         <img
--- islands/Footer.tsx
+++ islands/Footer.tsx
@@ -2,13 +2,13 @@ const Footer = () => {
   return (
     <div class="absolute bottom-0 left-0 flex px-4 justify-between w-full z-10">
       <span>
-        <a href="/" class='flex justify-center item-center gap-x-4'>
+        <a href="/" class='flex justify-center gap-x-4'>
           <img
-          class="h-8 mx-auto mb-4 drop-shadow-2xl"
-          src="/imgs/logoImage.svg"
+          class="h-4 mx-auto mb-4 -mr-2 drop-shadow-2xl"
+          src="/imgs/logoVec@3x.svg"
           alt="Logo"
         />
-        <span class='text-white text-2xl font-semibold'>Twinverse™</span>
+        <span class='text-white text-2xl -mt-2 font-semibold'>Twinverse™</span>
         </a>
       </span>
       <footer class="mr-10 flex gap-x-12 font-semibold text-gray-400">
