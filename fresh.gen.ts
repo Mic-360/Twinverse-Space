884a6876f7a58539882da5e24781b1d7f094fc9a
Bhaumic <bhaumiksingh2000@gmail.com>
2023-06-11 16:44:27 +0000

    chore: deno code default changes

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -7,6 +7,7 @@ import * as $0 from "./routes/api/joke.ts";
 import * as $1 from "./routes/index.tsx";
 import * as $2 from "./routes/mission/index.tsx";
 import * as $3 from "./routes/team/index.tsx";
+import * as $4 from "./routes/waitlist/index.tsx";
 import * as $$0 from "./islands/Footer.tsx";
 import * as $$1 from "./islands/Video.tsx";
 
@@ -16,6 +17,7 @@ const manifest = {
     "./routes/index.tsx": $1,
     "./routes/mission/index.tsx": $2,
     "./routes/team/index.tsx": $3,
+    "./routes/waitlist/index.tsx": $4,
   },
   islands: {
     "./islands/Footer.tsx": $$0,
