eb67ed201546dfebabc80eb65859bf37d7a9cc29
Bhaumic <bhaumiksingh2000@gmail.com>
2023-06-11 17:03:00 +0000

    fix: mission page improvements

--- routes/mission/index.tsx
+++ routes/mission/index.tsx
@@ -1,25 +1,23 @@
 import { Head } from "$fresh/runtime.ts";
+import Navbar from "../../components/Navbar.tsx";
 import Footer from "../../islands/Footer.tsx";
 import Video from "../../islands/Video.tsx";
 
 const Mission = () => {
   return (
     <>
-    <Head>
+      <Head>
         <title>Mission - Twinverse™</title>
         <link rel="icon" href="/imgs/logoImage.svg" />
         <link rel="stylesheet" href="globals.css" />
       </Head>
       <Video src="/videos/background.mp4" type="video/mp4" />
-      <div class="flex flex-col justify-center items-center h-screen text-gray-600 drop-shadow-[0_5px_3px_rgba(255,255,255,0.8)]">
-        <div>
-          <div class="mb-16 mx-8 text-center">
-            <span class="block text-5xl font-bold text-gray-300 mb-3 hover:text-white">
-              Our Mission.
-            </span>
-          </div>
-          <div class="w-full px-48 text-center tracking-widest leading-loose font-semibold text-xl text-white flex justify-center items-center flex-col gap-y-8">
-            <article>
+      <Navbar />
+      <div class="flex justify-center items-center h-screen text-gray-600 drop-shadow-[0_5px_3px_rgba(255,255,255,0.8)]">
+        <div class='flex flex-col justify-center items-center'>
+          <div class="mb-4 mt-16 mx-8 text-center w-2/3">
+            <h1 class="text-xl md:text-3xl lg:text-5xl mb-10">Our Mission.</h1>
+            <article class='text-md md:text-xl lg:text-2xl text-center'>
               To develop a software platform that allows users to easily create
               and customize their own virtual spaces without the need for
               extensive frontend, backend, and design development. Our software
@@ -27,29 +25,35 @@ const Mission = () => {
               it accessible and straightforward for anyone to create their own
               immersive experiences.
             </article>
-            <span class="flex justify-between text-center gap-x-20 mt-12">
-              <a target='_blank' href="https://www.facebook.com/twinversetech/">
+            <span class="flex justify-evenly text-center mt-12">
+              <a target="_blank" href="https://www.facebook.com/twinversetech/">
                 <img
                   class="h-10 w-10 mx-auto mb-4 drop-shadow-2xl cursor-pointer"
                   src="/imgs/facebook.svg"
                   alt="facebook"
                 />
               </a>
-              <a target='_blank' href="https://www.twitter.com/twinversetech/">
+              <a target="_blank" href="https://www.twitter.com/twinversetech/">
                 <img
                   class="h-10 w-10 mx-auto mb-4 drop-shadow-2xl cursor-pointer"
                   src="/imgs/twitter.svg"
                   alt="twitter"
                 />
               </a>
-              <a target='_blank' href="https://www.linkedin.com/company/twinversetech/">
+              <a
+                target="_blank"
+                href="https://www.linkedin.com/company/twinversetech/"
+              >
                 <img
                   class="h-10 w-10 mx-auto mb-4 drop-shadow-2xl cursor-pointer"
                   src="/imgs/linkedin.svg"
                   alt="linkedin"
                 />
               </a>
-              <a target='_blank' href="https://www.instagram.com/twinversetech/">
+              <a
+                target="_blank"
+                href="https://www.instagram.com/twinversetech/"
+              >
                 <img
                   class="h-10 w-10 mx-auto mb-4 drop-shadow-2xl cursor-pointer"
                   src="/imgs/instagram.svg"
