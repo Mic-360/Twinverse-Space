7edfda99dafbea6c5de88fc8de1129816fb2ab53
Bhaumic-kun <bhaumiksingh2000@gmail.com>
2023-09-04 04:33:24 +0530

    feat: form action to typeform
    Signed-off-by: Bhaumic-kun <bhaumiksingh2000@gmail.com>

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,6 +1,6 @@
 import { Head } from "$fresh/runtime.ts";
 import Button from "../components/Button.tsx";
-import Navbar from "../components/Navbar.tsx";
+import Navbar from "../islands/Navbar.tsx";
 import Footer from "../islands/Footer.tsx";
 import Video from "../islands/Video.tsx";
 
@@ -15,18 +15,13 @@ export default function Home() {
       <Video src="/videos/background.mp4" type="video/mp4" />
       <Navbar />
       <div class="flex justify-center items-center h-screen text-gray-600 drop-shadow-[0_5px_3px_rgba(255,255,255,0.8)]">
-        <div>
-          <div class="mb-4 mt-16 mx-8 text-center">
-            <span class="block text-2xl md:text-3xl lg:text-5xl font-bold text-gray-300 mb-3 hover:text-white">
-              Build Beyond Reality
-            </span>
-            <form
-              action={/* @__PURE__ */ "/waitlist"}
-              class="flex justify-end mt-8 mr-2"
-            >
-              <Button />
-            </form>
-          </div>
+        <div class="mb-4 mt-16 mx-8 text-center">
+          <span class="block text-2xl md:text-3xl lg:text-5xl font-bold text-gray-300 mb-8 hover:text-white">
+            Build Beyond Reality
+          </span>
+          <form action='https://896uznc175v.typeform.com/to/j8nQFo6f'>
+            <Button />
+          </form>
         </div>
       </div>
       <Footer />
