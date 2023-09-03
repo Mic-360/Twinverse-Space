f15d0fc23a42e6f0e668c155846e2ff0f645f623
Bhaumic-kun <bhaumiksingh2000@gmail.com>
2023-09-04 04:34:39 +0530

    feat: waitlist button with typeform
    Signed-off-by: Bhaumic-kun <bhaumiksingh2000@gmail.com>

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,19 +1,19 @@
+// Desc: Button component
 const Button = () => {
   return (
-    <button type='submit' class="flex w-full justify-center items-center flex-col rounded-lg border-gray-400 border-2">
-      <a
-        target='_blank'
-        href="/waitlist"
-        class="text-gray-400 hover:text-gray-100 tracking-tightest shadow font-bold text-sm py-1 px-1 md:py-2 lg:py-3 md:px-1.5 lg:px-2 flex justify-start items-center cursor-pointer animate-pulse"
-      >
+    <button
+      type={"submit"}
+      className="flex w-full justify-center items-center flex-col rounded-lg border-gray-400 border-2"
+    >
+      <div className="text-gray-400 hover:text-gray-100 tracking-tightest shadow font-bold text-sm py-1 px-1 md:py-2 lg:py-3 md:px-1.5 lg:px-2 flex justify-start items-center cursor-pointer animate-pulse">
         <span>Join The Waitlist</span>
         <img
-          class="fill-current -ml-4 w-10 h-6"
+          className="fill-current -ml-4 w-10 h-6"
           src="/imgs/arrow.svg"
           alt="assign"
         />
-      </a>
+      </div>
     </button>
   );
-}
+};
 export default Button;
