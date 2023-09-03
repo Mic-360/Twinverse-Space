5c55429355a7af7ae7ad622d26fb67c8fdcd03ae
Bhaumic-kun <bhaumiksingh2000@gmail.com>
2023-09-04 04:28:23 +0530

    fix: removed waitlist & made nav island

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -6,9 +6,10 @@ import * as $0 from "./routes/api/joke.ts";
 import * as $1 from "./routes/index.tsx";
 import * as $2 from "./routes/mission/index.tsx";
 import * as $3 from "./routes/team/index.tsx";
-import * as $4 from "./routes/waitlist/index.tsx";
+// import * as $4 from "./routes/waitlist/index.tsx";
 import * as $$0 from "./islands/Footer.tsx";
-import * as $$1 from "./islands/Video.tsx";
+import * as $$1 from "./islands/Navbar.tsx";
+import * as $$2 from "./islands/Video.tsx";
 
 const manifest = {
   routes: {
@@ -16,11 +17,12 @@ const manifest = {
     "./routes/index.tsx": $1,
     "./routes/mission/index.tsx": $2,
     "./routes/team/index.tsx": $3,
-    "./routes/waitlist/index.tsx": $4,
+    // "./routes/waitlist/index.tsx": $4,
   },
   islands: {
     "./islands/Footer.tsx": $$0,
-    "./islands/Video.tsx": $$1,
+    "./islands/Navbar.tsx": $$1,
+    "./islands/Video.tsx": $$2,
   },
   baseUrl: import.meta.url,
 };
