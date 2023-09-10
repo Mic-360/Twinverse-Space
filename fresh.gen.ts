f5105bd89d8dc495a2da8a8dbf4c92d898060edb
Bhaumic-kun <bhaumiksingh2000@gmail.com>
2023-09-10 08:13:36 +0530

    chore: fresh update
    Signed-off-by: Bhaumic-kun <bhaumiksingh2000@gmail.com>

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -2,22 +2,26 @@
 // This file SHOULD be checked into source version control.
 // This file is automatically updated during development when running `dev.ts`.
 
-import * as $0 from "./routes/api/joke.ts";
-import * as $1 from "./routes/index.tsx";
-import * as $2 from "./routes/mission/index.tsx";
-import * as $3 from "./routes/team/index.tsx";
-// import * as $4 from "./routes/waitlist/index.tsx";
+import * as $0 from "./routes/_app.tsx";
+import * as $1 from "./routes/api/joke.ts";
+import * as $2 from "./routes/demo/index.tsx";
+import * as $3 from "./routes/index.tsx";
+import * as $4 from "./routes/mission/index.tsx";
+import * as $5 from "./routes/team/index.tsx";
+import * as $6 from "./routes/waitlist/index.tsx";
 import * as $$0 from "./islands/Footer.tsx";
 import * as $$1 from "./islands/Navbar.tsx";
 import * as $$2 from "./islands/Video.tsx";
 
 const manifest = {
   routes: {
-    "./routes/api/joke.ts": $0,
-    "./routes/index.tsx": $1,
-    "./routes/mission/index.tsx": $2,
-    "./routes/team/index.tsx": $3,
-    // "./routes/waitlist/index.tsx": $4,
+    "./routes/_app.tsx": $0,
+    "./routes/api/joke.ts": $1,
+    "./routes/demo/index.tsx": $2,
+    "./routes/index.tsx": $3,
+    "./routes/mission/index.tsx": $4,
+    "./routes/team/index.tsx": $5,
+    "./routes/waitlist/index.tsx": $6,
   },
   islands: {
     "./islands/Footer.tsx": $$0,
